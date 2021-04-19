59aad4e20d8c037ab62d43af135e93d089fd4e56
Tawane Teles <73786592+Tawane-Teles@users.noreply.github.com>
2021-04-19 17:05:15 -0300

    criando layout para adapter e setando as view

--- app/src/main/java/com/example/mylist/MainActivity.kt
+++ app/src/main/java/com/example/mylist/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapterPais = PaaisesAdapter()
+    var listPaaises : RecyclerView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupViews()
+    }
+
+    private fun setupViews() {
+      listPaaises = findViewById(R.id.listPaaises)
+    }
+    private fun setupRecyclerView() {
+        with(listPaaises){
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/mylist/PaaisesAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.mylist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mylist.model.Pais
+import kotlin.properties.Delegates
+
+class PaaisesAdapter : RecyclerView.Adapter<PaaisesViewHolder>() {
+    val item: List<Pais> by Delegates.observable(emptyList()) { _, old, new -> if (old != new) notifyDataSetChanged() }
+    val paisitem: List<Pais> by Delegates.observable(emptyList()) { _, old, new -> if (old != new) notifyDataSetChanged() }
+    val filterItem: List<Pais> by Delegates.observable(emptyList()) { _, old, new -> if (old != new) notifyDataSetChanged() }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaaisesViewHolder {
+        return PaaisesViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_pais, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+    override fun onBindViewHolder(holder: PaaisesViewHolder, position: Int) {
+        val pais = item[position]
+        holder.bind(pais)
+    }
+}
+
+class PaaisesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(item: Pais) {
+
+        val text = itemView.findViewById<TextView>(R.id.textView)
+        text.text = item.name
+
+    }
+}
